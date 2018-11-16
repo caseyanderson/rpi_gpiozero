551a1a7b39eb86b54a7428958fb42e0065957868
caseyanderson <cta@caseyanderson.com>
2018-11-15 16:35:28 -0800

    refactor, add argparse

--- button_counter.py
+++ button_counter.py
@@ -13,28 +13,39 @@ button presses are identified when button.value is True and also diff state from
 from gpiozero import Button
 from gpiozero import LED
 from time import sleep
+import argparse
 
-button = Button(16)
-led = LED(17)
+if __name__ == '__main__':
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--btn", type=int, default=16,
+            help="The button pin")
+        parser.add_argument("--led", type=int, default=17,
+            help="The LED pin")
+        parser.add_argument("--limit", type=int, default=5,
+            help="The counter limit")
+        args = parser.parse_args()
 
-print("ready!")
-led.on()
+        button = Button(args.btn)
+        led = LED(args.led)
 
-counter = 0
-limit = 5
-prev_val = button.value
+        print("ready!")
+        led.on()
 
-try:
-    while True:
-        if button.value == True and button.value != prev_val:
-            counter += 1
-            print("counter is " + str(counter))
-            if counter == limit:
-                print("counter at limit, reset to 0")
-                counter = 0
+        counter = 0
+        limit = args.limit
         prev_val = button.value
-        sleep(0.01)
-except KeyboardInterrupt:
-    print("interrupted!")
-    button.close()
-    led.close()
+
+        while True:
+            if button.value == True and button.value != prev_val:
+                counter += 1
+                print("counter is " + str(counter))
+                if counter == limit:
+                    print("counter at limit, reset to 0")
+                    counter = 0
+            prev_val = button.value
+            sleep(0.01)
+    except KeyboardInterrupt:
+        print("interrupted!")
+        button.close()
+        led.close()
